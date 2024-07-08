eb1db48daa1c8af108c892841f383cb79b93dae7
GusTenshou <gustavo.x.amaral7@gmail.com>
2024-07-07 22:54:04 -0300

    Tentativa de testes

--- hardhat/contracts/energymarket.sol
+++ hardhat/contracts/energymarket.sol
@@ -2,32 +2,32 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-contract energyMarket {
-    mapping (address => Vendor) public vendors;
+contract energymarket {
+    mapping(address => Vendor) public vendors;
 
     address owner;
     uint energyCost;
-    struct Vendor {                     
+    struct Vendor {
         uint saldo;
         uint tax;
         uint dailyCapacity; // Média
         uint remainingCapacity;
     }
 
-    constructor( uint _energyCost ) {
+    constructor(uint _energyCost) {
         energyCost = _energyCost;
         owner = msg.sender;
     }
 
-
     function buyEnergy(address vendor, uint amount) external payable {
         uint price;
-        if(vendors[vendor].remainingCapacity >= amount) {
-            vendors[vendor].remainingCapacity = vendors[vendor].remainingCapacity - amount;
+        if (vendors[vendor].remainingCapacity >= amount) {
+            vendors[vendor].remainingCapacity =
+                vendors[vendor].remainingCapacity -
+                amount;
             price = amount * energyCost;
             vendors[vendor].remainingCapacity -= amount;
-        }
-            else {
+        } else {
             price = vendors[vendor].remainingCapacity * energyCost;
             amount -= vendors[vendor].remainingCapacity;
             price += amount * vendors[vendor].tax * energyCost;
@@ -36,33 +36,36 @@ contract energyMarket {
 
         require(msg.value >= price, "Insufficient funds sent");
         vendors[vendor].saldo += price;
-    
-    
-        if (msg.value > price) { /* TROCO */
+
+        if (msg.value > price) {
+            /* TROCO */
             payable(msg.sender).transfer(msg.value - price);
         }
     }
 
-    function changeCapacity(address vendorAddress, uint newCapacity) public{
+    function changeCapacity(address vendorAddress, uint newCapacity) public {
         require(msg.sender == vendorAddress, "Incorrect address");
         vendors[vendorAddress].dailyCapacity = newCapacity;
     }
 
-    function addVendor ( address newVendor, uint newCapacity, uint newTax) public {
-        require( msg.sender == owner, "You need to be the owner to do this" );
-            vendors[newVendor].dailyCapacity = newCapacity;
-            vendors[newVendor].tax = newTax;
-            vendors[newVendor].remainingCapacity = newCapacity;
-            vendors[newVendor].saldo = 0;
-        
+    function addVendor(
+        address newVendor,
+        uint newCapacity,
+        uint newTax
+    ) public {
+        require(msg.sender == owner, "You need to be the owner to do this");
+        vendors[newVendor].dailyCapacity = newCapacity;
+        vendors[newVendor].tax = newTax;
+        vendors[newVendor].remainingCapacity = newCapacity;
+        vendors[newVendor].saldo = 0;
     }
 
     function Withdraw() public payable {
-         payable(msg.sender).transfer(vendors[msg.sender].saldo);
+        payable(msg.sender).transfer(vendors[msg.sender].saldo);
     }
 
     function setEnergyCost(uint newEnergyCost) public {
-        require(msg.sender == owner, "You need to be the owner to do this" );
+        require(msg.sender == owner, "You need to be the owner to do this");
         energyCost = newEnergyCost;
     }
 
