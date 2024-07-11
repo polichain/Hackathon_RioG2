3681985be73c22939c558eb189255024d6981a5b
GusTenshou <gustavo.x.amaral7@gmail.com>
2024-07-11 19:12:34 -0300

    melhorias

--- next/hackathonrio/src/app/client/page.tsx
+++ next/hackathonrio/src/app/client/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 
-export default function page() {
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Buy Energy
-      <header>
-        <button>Select vendor</button>
-      </header>
-      <header></header>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="text-xl mb-8">Buy Energy</div>
+      <div className="flex flex-col items-center space-y-4">
+        <header>
+          <button className="px-4 py-2 border rounded">Select Vendor</button>
+        </header>
+      </div>
     </main>
   );
 }
--- next/hackathonrio/src/app/vendor/page.tsx
+++ next/hackathonrio/src/app/vendor/page.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 
-export default function page() {
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      vendor page
-      <header>
-        <button>Already vendor</button>
-      </header>
-      <header>
-        <button>Become a vendor</button>
-      </header>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="text-xl mb-8">Vendor Page</div>
+      <div className="flex flex-col items-center space-y-4">
+        <header>
+          <button className="px-4 py-2 border rounded">Already Vendor</button>
+        </header>
+        <header>
+          <button className="px-4 py-2 border rounded">Become a Vendor</button>
+        </header>
+      </div>
     </main>
   );
 }
