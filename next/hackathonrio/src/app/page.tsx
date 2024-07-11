1b097e732de81608a7884833e541bc4eb67dfe8a
GusTenshou <gustavo.x.amaral7@gmail.com>
2024-07-11 19:12:34 -0300

    melhorias

--- next/hackathonrio/src/app/page.tsx
+++ next/hackathonrio/src/app/page.tsx
@@ -3,18 +3,20 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Energy Market
-      <header>
-        <Link href="/client">
-          <button>Buy Energy</button>
-        </Link>
-      </header>
-      <header>
-        <Link href="/vendor">
-          <button>Vendor page</button>
-        </Link>
-      </header>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="text-xl mb-8">Energy Market</div>
+      <div className="flex space-x-4">
+        <header>
+          <Link href="/client">
+            <button className="px-4 py-2 border rounded">Buy Energy</button>
+          </Link>
+        </header>
+        <header>
+          <Link href="/vendor">
+            <button className="px-4 py-2 border rounded">Vendor</button>
+          </Link>
+        </header>
+      </div>
     </main>
   );
 }
