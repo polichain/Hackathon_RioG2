71268f014930978e06764dc9491431b2376fc5e1
GusTenshou <gustavo.x.amaral7@gmail.com>
2024-07-11 18:53:26 -0300

    Next with pages added

---
+++ next/hackathonrio/src/app/client/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Buy Energy
+      <header>
+        <button>Select vendor</button>
+      </header>
+      <header></header>
+    </main>
+  );
+}
---
+++ next/hackathonrio/src/app/vendor/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      vendor page
+      <header>
+        <button>Already vendor</button>
+      </header>
+      <header>
+        <button>Become a vendor</button>
+      </header>
+    </main>
+  );
+}
