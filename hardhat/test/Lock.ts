2bdd3725abb6a4b53ab7aaf2a9296166794e8833
Kiyochi <shojidan@gmail.com>
2024-07-12 18:53:15 -0300

    teste

--- hardhat/test/Lock.ts
+++ hardhat/test/Lock.ts
@@ -13,14 +13,13 @@ describe("Lock", function () {
   async function deployOneYearLockFixture() {
     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
 
-    const lockedAmount = parseGwei("1"); // 1 Gwei
-    const unlockTime = BigInt((await time.latest()) + ONE_YEAR_IN_SECS); // Tempo
+    const lockedAmount = parseGwei("1");
+    const unlockTime = BigInt((await time.latest()) + ONE_YEAR_IN_SECS);
 
     // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await hre.viem.getWalletClients();
 
     const lock = await hre.viem.deployContract("Lock", [unlockTime], {
-      // deploy, unlockTime como variavel de entrada e lockedAmount como dinheiro payable
       value: lockedAmount,
     });
 
