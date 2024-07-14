d1a0ca54367aac8d4f3fff75ec01916f1e3c3e70
GusTenshou <gustavo.x.amaral7@gmail.com>
2024-07-13 22:41:18 -0300

    mudanças no vendor

--- next/hackathonrio/src/app/vendor/registrated/page.tsx
+++ next/hackathonrio/src/app/vendor/registrated/page.tsx
@@ -1,15 +1,15 @@
 "use client";
-
+import { useState } from "react";
 import Image from "next/image";
-import React, { useState } from "react";
 
 export default function Page() {
   const [capacity, setCapacity] = useState<number | "">("");
-  const [tax, setTax] = useState<number | "">("");
 
   const handleSetCapacity = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCapacity(event.target.value === "" ? "" : Number(event.target.value));
   };
+
+  const [tax, setTax] = useState<number | "">("");
 
   const handleSetTax = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTax(event.target.value === "" ? "" : Number(event.target.value));
@@ -17,10 +17,13 @@ export default function Page() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="text-xl mb-8">Vendor Registration</div>
+      <div className="text-xl mb-8">Vendor Page</div>
       <div className="flex flex-col items-center space-y-4">
         <header>
-          <form>
+          <button className="px-4 py-2 border rounded">Withdrawn</button>
+        </header>
+        <header>
+          <form name="capacityForm">
             <input
               type="number"
               value={capacity}
@@ -28,6 +31,10 @@ export default function Page() {
               placeholder="Capacity"
               className="px-4 py-2 border rounded"
             />
+          </form>
+        </header>
+        <header>
+          <form name="taxForm">
             <input
               type="number"
               value={tax}
